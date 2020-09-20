dbfb154370632a4ce08dccf3c7ff89ddcd899615
Jaivardhan Kumar <mailjai.vardhan@gmail.com>
2020-09-20 19:32:14 +0530

    adds api for signup/login

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,15 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const router = express.Router();
+
+const saltRadius = 10;
 
 //Get user list
 router.get("/", async (req, res) => {
   try {
     const user = await User.find();
     res.json(user);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
-
-//Submit user data
-router.post("/", async (req, res) => {
-  const user = new User({
-    userName: req.body.userName,
-    password: req.body.password,
-  });
-  try {
-    const addedUser = await user.save();
-    res.json(addedUser);
   } catch (err) {
     res.json({ message: err });
   }
@@ -57,6 +46,72 @@ router.delete("/:userName", async (req, res) => {
   } catch (err) {
     res.json({ message: err });
   }
+});
+
+//Signup user
+router.post("/signup", async (req, res) => {
+  const newUser = new User({
+    userName: req.body.userName,
+    password: req.body.password,
+  });
+
+  await User.findOne({ userName: newUser.userName })
+    .then(async (profile) => {
+      if (!profile) {
+        bcrypt.hash(newUser.password, saltRadius, async (err, hash) => {
+          if (err) {
+            console.log(err.message);
+          } else {
+            newUser.password = hash;
+            await newUser
+              .save()
+              .then(() => {
+                res.status(200).send(newUser);
+              })
+              .catch((err) => {
+                console.log("Error is ", err.message);
+              });
+          }
+        });
+      } else {
+        res.send("User already exists...");
+      }
+    })
+    .catch((err) => {
+      console.log("Error is", err.message);
+    });
+});
+
+//Log In User
+router.post("/login", async (req, res) => {
+  const newUser = new User({
+    userName: req.body.userName,
+    password: req.body.password,
+  });
+
+  await User.findOne({ userName: newUser.userName })
+    .then((profile) => {
+      if (!profile) {
+        res.send("User not exist");
+      } else {
+        bcrypt.compare(
+          newUser.password,
+          profile.password,
+          async (err, result) => {
+            if (err) {
+              console.log("Error is", err.message);
+            } else if (result == true) {
+              res.send("User authenticated");
+            } else {
+              res.send("User Unauthorized Access");
+            }
+          }
+        );
+      }
+    })
+    .catch((err) => {
+      console.log("Error is ", err.message);
+    });
 });
 
 module.exports = router;
